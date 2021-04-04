539b743f16b7f9410def249aa16133c7579ce12d
Steven Lee <lees106@users.noreply.github.com>
2021-04-04 13:14:59 -0700

    updated nav bar and implemented landing page

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA_x7lIPYOydzLmnIEh7dTElMe_l6gCrYE",
+  authDomain: "clone-1d540.firebaseapp.com",
+  projectId: "clone-1d540",
+  storageBucket: "clone-1d540.appspot.com",
+  messagingSenderId: "1074619628240",
+  appId: "1:1074619628240:web:3811a129c91c38bf6d3cd5",
+  measurementId: "G-X5BQ8W5LBM"
+};
